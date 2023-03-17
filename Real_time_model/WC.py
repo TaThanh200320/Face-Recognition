549b89552dbc38528e11ae5af82046ec5ca1b12c
Tạ Đình Thành <127477315+TaThanh200320@users.noreply.github.com>
2023-03-17 08:50:18 +0700

    Update WC.py

--- Real_time_model/WC.py
+++ Real_time_model/WC.py
@@ -4,8 +4,8 @@ import os
 from sklearn.preprocessing import LabelEncoder
 import pickle as pkl
 from keras_facenet import FaceNet
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 facenet = FaceNet()
 faces_embeddings = np.load("Faces_embedded_2class.npz")
 Y = faces_embeddings['arr_1']
@@ -16,24 +16,24 @@ model = pkl.load(open("svm_model.pkl", 'rb'))
 
 cap = cv.VideoCapture(0)
 
-
 while cap.isOpened():
-    _, frame = cap.read()
-    rgb_img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    ret, frame = cap.read()
+    if not ret:
+        break
     gray_img = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     faces = haarcascade.detectMultiScale(gray_img, 1.3, 3)
-    for x,y,w,h in faces:
-        img = rgb_img[y:y+h, x:x+w]
-        img = cv.resize(img, (160,160))
-        img = np.expand_dims(img,axis=0)
-        ypred = facenet.embeddings(img)
-        face_name = model.predict(ypred)
-        final_name = encoder.inverse_transform(face_name)[0]
-
-        cv.rectangle(frame, (x,y), (x+w,y+h), (255,0,255), 5)
-        cv.putText(frame, str(final_name), (x,y-10), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 3, cv.LINE_AA)
-
-    cv.imshow("Face Recognition:", frame)
+    face_imgs = []
+    for x, y, w, h in faces:
+        face_img = frame[y:y+h, x:x+w]
+        face_imgs.append(cv.resize(face_img, (160, 160)))
+    if len(face_imgs) > 0:
+        face_imgs = np.stack(face_imgs)
+        face_embeddings = facenet.embeddings(face_imgs)
+        face_names = encoder.inverse_transform(model.predict(face_embeddings))
+    for i, (x, y, w, h) in enumerate(faces):
+        cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 255), 5)
+        cv.putText(frame, str(face_names[i]), (x, y-10), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3, cv.LINE_AA)
+    cv.imshow("Face Recognition", frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
